390e7ba2cc111e06e56437b3d0d3d295d3fff11d
juliettaurdiales@yahoo.comlsgit statusgit commit README.mdcd /var/www/mtbclsgit config --global user.name juliettaurdiales@yahoo.com <juliettaurdiales@yahoo.com>
2017-12-15 10:12:08 -0600

    added new file structure

--- public/contact.php
+++ public/contact.php
@@ -6,11 +6,34 @@
        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <div>
+        <nav>
     <a href="/">Home</a>  |
     <a href="resume.html">Resume</a>  |
     <a href="contact.php">Contact</a>
-  </div>
+</nav>
       Contact
-    </body>
+      <div>
+              <label for="firstName">First Name</label><br>
+              <input type="text" name="first_name" id="firstName">
+            </div>
+
+            <div>
+              <label for="lastName" id="lastName">Last Name</label><br>
+              <input type="text" name="last_name">
+            </div>
+
+            <div>
+              <label for="email" id="email">Email</label><br>
+              <input type="text" name="email">
+            </div>
+
+            <div>
+              <label for="subject" id="subject">Subject</label><br>
+              <input type="text" name="subject">
+            </div>
+
+            <div>
+              <label for="message" id="message">Message</label><br>
+              <textarea name="message"></textarea>
+            </div>    </body>
 </html>
